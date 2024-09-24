57c4d47f7f8ea0c275b69c24485f9421909d0c9f
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-09-24 13:04:00 +0600

    debugging

---
+++ js/debugging.js
@@ -0,0 +1,14 @@
+// why we need debugging?
+/*
+* we need debugging to find any error in our code
+* to test our code, to enhance our code, to improve optimize code we need debugging
+ */
+
+// How we can debug our code?
+/**
+ * debugging using cosole.log
+ * debugging using browser debug tool
+ * debug using browser debugger keyword
+ * Explore browser network tab
+ * debugging on node js runtime
+ */
